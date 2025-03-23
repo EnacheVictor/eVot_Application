5119cdd09ad18bfd54af40b82cfde3e4daf4a07f
EnacheVictor <enachevictor887@gmail.com>
2025-03-23 16:08:02 +0200

    Log in activity integration

---
+++ app/src/main/java/com/victor/evotapplication/LogInActivity.kt
@@ -0,0 +1,24 @@
+package com.victor.evotapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_in)
+
+    }
+}
--- app/src/main/java/com/victor/evotapplication/MainActivity.kt
+++ app/src/main/java/com/victor/evotapplication/MainActivity.kt
@@ -2,25 +2,80 @@ package com.victor.evotapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.victor.evotapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.createAccBTN.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
+            var intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+        binding.emailSignInButton.setOnClickListener {
+            signInWithEmailAndPassword(
+                binding.email.text.toString().trim(),
+                binding.password.text.toString().trim()
+            )
+        }
+        }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            goToLogInActivity() // te pastreaza conectat
         }
     }
+
+    fun goToLogInActivity() {
+        var intent = Intent(this, LogInActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun signInWithEmailAndPassword(email: String , password:String)
+    {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAGY", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication success.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    goToLogInActivity()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAGY", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+}
+
